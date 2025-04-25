db0588dac04a921c7a37388e4b485e32ee8bc35f
shubh <shubham2932@gmail.com>
2025-04-26 03:23:04 +0530

    Implement connect and disconnect functions

--- chess-connect-function/src/main/java/com/crushedlemon/chess/ChessConnectFunction.java
+++ chess-connect-function/src/main/java/com/crushedlemon/chess/ChessConnectFunction.java
@@ -1,23 +1,44 @@
 package com.crushedlemon.chess;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.util.Map;
 
 public class ChessConnectFunction implements RequestHandler<Map<String, Object>, Object> {
 
+    private final DynamoDB dynamoDB;
+    private final Table table;
+
+    public ChessConnectFunction() {
+        this.dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.standard().build());
+        this.table = dynamoDB.getTable("chess-connections");
+    }
+
     @Override
     public Object handleRequest(Map<String, Object> event, Context context) {
-        context.getLogger().log("User is now connected!");
-        for (Map.Entry e : event.entrySet()) {
-            context.getLogger().log("========PARAMS ARE=========");
-            context.getLogger().log(e.getKey().toString());
-            context.getLogger().log(e.getValue().toString());
-            context.getLogger().log("****************");
-        }
-        context.getLogger().log("========CONTEXT=========");
-        context.getLogger().log(context.toString());
-        context.getLogger().log("****************");
+        String connectionId = extractConnectionId(event);
+        String userName = extractUserName(event);
+        saveConnection(userName, connectionId);
         return Map.of("statusCode", 200);
+    }
+
+    private void saveConnection(String userName, String connectionId) {
+        this.table.putItem(Item.fromMap(Map.of("userId", userName, "connectionId", connectionId, "status", "CONNECTED")));
+    }
+
+    // TODO : move this to a common class
+    private static String extractConnectionId(Map<String, Object> event) {
+        Map<String, Object> requestContext = (Map<String, Object>) event.get("requestContext");
+        return (String) requestContext.get("connectionId");
+    }
+
+    private static String extractUserName(Map<String, Object> event) {
+        // For testing purposes, passing the username in query params. Change it to JWT-based identity.
+        Map<String, Object> queryStringParameters = (Map<String, Object>) event.get("queryStringParameters");
+        return (String) queryStringParameters.get("userName");
     }
 }
--- chess-disconnect-function/src/main/java/com/crushedlemon/chess/ChessDisconnectFunction.java
+++ chess-disconnect-function/src/main/java/com/crushedlemon/chess/ChessDisconnectFunction.java
@@ -1,15 +1,49 @@
 package com.crushedlemon.chess;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Iterator;
 import java.util.Map;
 
 public class ChessDisconnectFunction implements RequestHandler<Map<String, Object>, Object> {
 
+    private final DynamoDB dynamoDB;
+    private final Table table;
+
+    public ChessDisconnectFunction() {
+        this.dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.standard().build());
+        this.table = dynamoDB.getTable("chess-connections");
+    }
+
     @Override
     public Object handleRequest(Map<String, Object> event, Context context) {
-        context.getLogger().log("User is now disconnected!");
-        context.getLogger().log("And we know which one also");
+        String connectionId = extractConnectionId(event);
+        removeConnection(connectionId);
         return Map.of("statusCode", 200);
+    }
+
+    private void removeConnection(String connectionId) {
+        QuerySpec querySpec = new QuerySpec()
+                .withKeyConditionExpression("connectionId = :v_connection_id")
+                .withValueMap(new ValueMap().withString(":v_connection_id", connectionId));
+
+        Iterator<Item> iterator = table.getIndex("connectionId-index").query(querySpec).iterator();
+
+        if (iterator.hasNext()) {
+            this.table.deleteItem("userId", iterator.next().get("userId"));
+        }
+    }
+
+    // TODO : move this to a common class
+    private static String extractConnectionId(Map<String, Object> event) {
+        Map<String, Object> requestContext = (Map<String, Object>) event.get("requestContext");
+        return (String) requestContext.get("connectionId");
     }
 }

31a6c5b8b9e76b9f0c59d3d8a8abedb6ffced4e3
crushed-lemon <crushed.lemon.official@gmail.com>
2025-04-30 04:50:03 +0530

    Pass user name in the request body for testing phase

--- chess-lambda-commons/src/main/java/com/crushedlemon/chess/commons/Utils.java
+++ chess-lambda-commons/src/main/java/com/crushedlemon/chess/commons/Utils.java
@@ -1,10 +1,14 @@
 package com.crushedlemon.chess.commons;
 
 import com.crushedlemon.chess.commons.model.GamePreferences;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Map;
 
 public class Utils {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public static String extractConnectionId(Map<String, Object> event) {
         Map<String, Object> requestContext = (Map<String, Object>) event.get("requestContext");
@@ -12,9 +16,14 @@ public class Utils {
     }
 
     public static String extractUserName(Map<String, Object> event) {
-        // For testing purposes, passing the username in query params. Change it to JWT-based identity.
-        Map<String, Object> queryStringParameters = (Map<String, Object>) event.get("queryStringParameters");
-        return (String) queryStringParameters.get("userName");
+        // For testing purposes, passing the username in request body. Change it to JWT-based identity.
+        String bodyJson = (String) event.get("body");
+        try {
+            Map<String, Object> bodyMap = (Map<String, Object>) objectMapper.readValue(bodyJson, Map.class);
+            return  (String) bodyMap.get("user");
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static GamePreferences extractGamePreferences(Map<String, Object> event) {

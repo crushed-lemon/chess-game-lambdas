97959ca49f9c08e76d1155b5fe18b1fff2ca8487
crushed-lemon <crushed.lemon.official@gmail.com>
2025-04-30 05:21:53 +0530

    Extract Game Prefernces from body

--- chess-lambda-commons/src/main/java/com/crushedlemon/chess/commons/Utils.java
+++ chess-lambda-commons/src/main/java/com/crushedlemon/chess/commons/Utils.java
@@ -1,6 +1,9 @@
 package com.crushedlemon.chess.commons;
 
+import com.crushedlemon.chess.commons.model.GameDuration;
 import com.crushedlemon.chess.commons.model.GamePreferences;
+import com.crushedlemon.chess.commons.model.IncrementPerMove;
+import com.crushedlemon.chess.commons.model.PlayAs;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -32,7 +35,27 @@ public class Utils {
     }
 
     public static GamePreferences extractGamePreferences(Map<String, Object> event) {
-        // TODO : Extract the Game preferences
-        return GamePreferences.builder().build();
+        String bodyJson = (String) event.get("body");
+        try {
+            Map<String, Object> bodyMap = (Map<String, Object>) objectMapper.readValue(bodyJson, Map.class);
+            Map<String, Object> gamePreferences = (Map<String, Object>) bodyMap.get("gamePreferences");
+
+            String gameDurationString = (String) gamePreferences.get("gameDuration");
+            GameDuration gameDuration = GameDuration.valueOf(gameDurationString);
+
+            String incrementPerMoveString = (String) gamePreferences.get("incrementPerMove");
+            IncrementPerMove incrementPerMove = IncrementPerMove.valueOf(incrementPerMoveString);
+
+            String playAsString = (String) gamePreferences.get("playAs");
+            PlayAs playAs = PlayAs.valueOf(playAsString);
+
+            return GamePreferences.builder()
+                    .gameDuration(gameDuration)
+                    .incrementPerMove(incrementPerMove)
+                    .playAs(playAs)
+                    .build();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
